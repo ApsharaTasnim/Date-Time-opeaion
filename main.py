a6d37a73b83f44164a39ccd32fb698a0a482133b
ApsharaTasnim <alferdstanley@gmail.com>
2024-10-18 22:35:55 +0600

    add time

---
+++ main.py
@@ -0,0 +1,8 @@
+import datetime
+import time
+current_time=datetime.datetime.now()
+print("The current time at greenwich is:", end="")
+print(current_time)
+
+import calender
+print("\n",calender.calender(2024))
